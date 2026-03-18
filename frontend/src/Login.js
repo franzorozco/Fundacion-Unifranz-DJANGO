001ec6432c147caecc8648ae3373c60c4cc1ff3d
franzorozco <ftanzorozco2@gmail.com>
2026-03-18 03:09:32 -0400

    frontend integrado correctamente

---
+++ frontend/src/Login.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const response = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    });
+
+    const data = await response.json();
+
+    if (data.access) {
+      // 🔥 Guardar token
+      localStorage.setItem("token", data.access);
+      alert("Login exitoso 🚀");
+    } else {
+      alert("Error en login ❌");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Usuario"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Iniciar sesión</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
